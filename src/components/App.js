e0f00eb225d2fadf9ac1306bc165d158d6ac39b8
Clem <valles.clement@gmail.com>
2021-02-06 11:03:41 -0600

    add Welcome message

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div className="container">
+
+      <Welcome />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+function Welcome() {
+    return (
+  
+        <h2>Welcome Albatros fan?!</h2>
+
+    );
+  }
+
+  export default Welcome;
